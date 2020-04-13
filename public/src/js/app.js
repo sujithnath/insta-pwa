a5ebb1f7218c19fb7d6bbfdeafef6de98ec85f29
Sujith <sujith@Sujiths-MacBook-Pro.local>
2020-04-13 11:43:31 +0530

    intial phase

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,11 @@
+let deferredPrompt;
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then(function () {});
 }
+
+//Hiding the banner
+windiw.addEventListener('beforeinstallprompt', () => {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+});
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,9 +1,23 @@
 var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var closeCreatePostModalButton = document.querySelector(
+  '#close-create-post-modal-btn'
+);
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      console.log(choiceResult.outcome);
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('user cancelled installation');
+      } else {
+        console.log('user added to home screen');
+      }
+      deferredPrompt = null;
+    });
+  }
 }
 
 function closeCreatePostModal() {
