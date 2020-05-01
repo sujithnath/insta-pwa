44a3bc68ac47da5687efcace275578f02b4324b3
sujithnath <sujithgnth@gmail.com>
2020-05-01 09:35:12 +0530

    introduced web oush notificaiton adn image changes

--- public/src/js/promise.js
+++ public/src/js/promise.js
@@ -4,62 +4,61 @@
  * Released under the MIT license
  * github.com/Octane/setImmediate
  */
-window.setImmediate ||
-  (function () {
-    'use strict';
+window.setImmediate || function () {'use strict';
 
-    var uid = 0;
-    var storage = {};
-    var firstCall = true;
-    var slice = Array.prototype.slice;
-    var message = 'setImmediatePolyfillMessage';
+  var uid = 0;
+  var storage = {};
+  var firstCall = true;
+  var slice = Array.prototype.slice;
+  var message = 'setImmediatePolyfillMessage';
 
-    function fastApply(args) {
-      var func = args[0];
-      switch (args.length) {
-        case 1:
-          return func();
-        case 2:
-          return func(args[1]);
-        case 3:
-          return func(args[1], args[2]);
-      }
-      return func.apply(window, slice.call(args, 1));
+  function fastApply(args) {
+    var func = args[0];
+    switch (args.length) {
+      case 1:
+        return func();
+      case 2:
+        return func(args[1]);
+      case 3:
+        return func(args[1], args[2]);
     }
+    return func.apply(window, slice.call(args, 1));
+  }
 
-    function callback(event) {
-      var key = event.data;
-      var data;
-      if (typeof key == 'string' && key.indexOf(message) == 0) {
-        data = storage[key];
-        if (data) {
-          delete storage[key];
-          fastApply(data);
-        }
+  function callback(event) {
+    var key = event.data;
+    var data;
+    if (typeof key == 'string' && key.indexOf(message) == 0) {
+      data = storage[key];
+      if (data) {
+        delete storage[key];
+        fastApply(data);
       }
     }
+  }
 
-    window.setImmediate = function setImmediate() {
-      var id = uid++;
-      var key = message + id;
-      var i = arguments.length;
-      var args = new Array(i);
-      while (i--) {
-        args[i] = arguments[i];
-      }
-      storage[key] = args;
-      if (firstCall) {
-        firstCall = false;
-        window.addEventListener('message', callback);
-      }
-      window.postMessage(key, '*');
-      return id;
-    };
+  window.setImmediate = function setImmediate() {
+    var id = uid++;
+    var key = message + id;
+    var i = arguments.length;
+    var args = new Array(i);
+    while (i--) {
+      args[i] = arguments[i];
+    }
+    storage[key] = args;
+    if (firstCall) {
+      firstCall = false;
+      window.addEventListener('message', callback);
+    }
+    window.postMessage(key, '*');
+    return id;
+  };
 
-    window.clearImmediate = function clearImmediate(id) {
-      delete storage[message + id];
-    };
-  })();
+  window.clearImmediate = function clearImmediate(id) {
+    delete storage[message + id];
+  };
+
+}();
 
 /**
  * Promise polyfill v1.0.10
@@ -69,8 +68,7 @@ window.setImmediate ||
  * Released under the MIT license
  * github.com/Octane/Promise
  */
-(function (global) {
-  'use strict';
+(function (global) {'use strict';
 
   var STATUS = '[[PromiseStatus]]';
   var VALUE = '[[PromiseValue]]';
@@ -86,9 +84,7 @@ window.setImmediate ||
   var CHAINING_CYCLE = 'then() cannot return same Promise that it resolves.';
 
   var setImmediate = global.setImmediate || require('timers').setImmediate;
-  var isArray =
-    Array.isArray ||
-    function (anything) {
+  var isArray = Array.isArray || function (anything) {
       return Object.prototype.toString.call(anything) == '[object Array]';
     };
 
@@ -159,7 +155,7 @@ window.setImmediate ||
     if (isPromise(anything)) {
       return anything;
     }
-    if (isObject(anything)) {
+    if(isObject(anything)) {
       try {
         then = anything.then;
       } catch (error) {
@@ -193,7 +189,7 @@ window.setImmediate ||
     }
     try {
       resolver(resolve, reject);
-    } catch (error) {
+    } catch(error) {
       reject(error);
     }
   }
@@ -340,7 +336,7 @@ window.setImmediate ||
           if (isPromise(anything)) {
             promiseCount++;
             anything.then(
-              (function (index) {
+              function (index) {
                 return function (value) {
                   values[index] = value;
                   fulfilledCount++;
@@ -348,7 +344,7 @@ window.setImmediate ||
                     resolve(values);
                   }
                 };
-              })(i),
+              }(i),
               reject
             );
           } else if (isInternalError(anything)) {
@@ -372,4 +368,5 @@ window.setImmediate ||
   } else if (!global.Promise) {
     global.Promise = Promise;
   }
-})(this);
+
+}(this));
